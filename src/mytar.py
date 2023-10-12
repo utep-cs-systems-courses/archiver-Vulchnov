40be75a1137a9d2933ed3e77408f47657cc55f0a
Jeffrey Walls <fr0st3e5@gmail.com>
2023-10-12 17:37:42 -0600

    Final Implementation

--- src/mytar.py
+++ src/mytar.py
@@ -6,31 +6,38 @@ import os         # checking if file exists
 import string   
 
 if len(sys.argv) < 3:
-    print("Insufficient arguments")
+    os.write(2, "Insufficient arguments".encode())
     exit()
 cmd = sys.argv[1]
 
 if cmd == "c":
     for arg in sys.argv:
         if arg != "c" and arg != "./mytar.py":
-            data = b""
             inputFile = os.open(arg, os.O_RDONLY)
+            file_size = os.path.getsize(arg)
+            byte = str(len(arg))+":"+arg+str(file_size)+":"
+            byte = byte.encode()
+            os.write(1, byte)
             read = os.read(inputFile, 100)
             while read != b"":
-                data = data+read
+                os.write(1, read)
                 read = os.read(inputFile, 100)
-            byte = str(len(arg))+":"+arg+str(len(data))+":"
-            byte = byte.encode()
-            byte = byte + data
-            os.write(1, byte)
             os.close(inputFile)
 
 if cmd == "x":
     inputFile = os.open(sys.argv[2], os.O_RDONLY)
-    data = ""
-    read = os.read(inputFile, 100).decode()
+    read = os.read(inputFile, 1).decode()
     while read != "":
-        data = data+read
-        read = os.read(inputFile, 100).decode()
-    lines = data.split("\n")
-    
+        fNameSize = ""
+        fSize = ""
+        while read != ":":
+            fNameSize = fNameSize + read
+            read = os.read(inputFile, 1).decode()
+        fName = os.read(inputFile, int(fNameSize)).decode()
+        outputFile = os.open(fName, os.O_WRONLY | os.O_CREAT)
+        read = os.read(inputFile, 1).decode()
+        while read != ":":
+            fSize = fSize + read
+            read = os.read(inputFile, 1).decode()
+        os.write(outputFile, os.read(inputFile, int(fSize)))
+        read = os.read(inputFile, 1).decode()
